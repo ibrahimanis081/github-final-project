d06029328f061237f542ed45825fc8de52c09ed5
Muhammad Ibrahim Anis <ibrahimanis081@gmail.com>
2023-09-13 22:27:13 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # swapnil2008
+   # ibrahimanis081
    # Input:
    # p, principal amount
    # t, time period in years
